4e9159028039df5c8530a904afbd3f82cab947b9
Alexis Raya <aar335@drexel.edu>
2023-03-16 10:11:50 -0400

    remove background tab bar color

--- idm362-aar335/ViewController.swift
+++ idm362-aar335/ViewController.swift
@@ -94,10 +94,10 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             tableView.beginUpdates()
-            
+
             vitamins.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-            
+
             tableView.endUpdates()
         }
     }
